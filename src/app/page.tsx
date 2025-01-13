99638e2ba17f2fa4d1e707fd242354529377ee43
Saket Roy <saket.roy@intellemo.com>
2025-01-13 14:27:55 +0530

    update input component props to support input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { SH2 } from '@/design-system/typography';
 import { useFunction } from '@/hooks/use/useFunction';
 import { SvgChainPath } from './widgets/svg/SvgChainPath';
 import { Chip } from '@/design-system/chip';
+import { SvgChainNode } from './widgets/svg/SvgChainNode';
 
 export default function Home() {
     const { state, updateCardEquation, updateInitialValue, addCardPos, dispatch } = useFunction();
@@ -17,13 +18,20 @@ export default function Home() {
     };
 
     return (
-        <XStack className='py-24 justify-center'>
-            <Input
-                type={'number'}
-                label={<Chip className='bg-decorative-yellow-200'>Initial value x</Chip>}
-                onBlur={_updateInitialValue}
-            />
-            <XStack className='flex-wrap justify-around gap-y-20 gap-x-20'>
+        <XStack className='py-24 px-8 justify-center'>
+            <XStack className='self-center'>
+                <Input
+                    variant={'lg'}
+                    color={'yellow'}
+                    type={'number'}
+                    className='w-24'
+                    label={<Chip className='bg-decorative-yellow-200'>Initial value of x</Chip>}
+                    TrailingComponent={<SvgChainNode />}
+                    onBlur={_updateInitialValue}
+                />
+                <XStack className='self-end'></XStack>
+            </XStack>
+            <XStack className='flex-wrap justify-around gap-y-20 gap-x-12'>
                 {state.data.map((item, index) => (
                     <FunctionCard
                         key={index}
@@ -32,19 +40,22 @@ export default function Home() {
                         dispatch={dispatch}
                     />
                 ))}
-
                 <svg className='absolute top-0 left-0 w-full h-full pointer-events-none'>
                     {state?.linePaths?.map((path, index) => (
                         <SvgChainPath key={index} d={path} />
                     ))}
                 </svg>
             </XStack>
-            <YStack>
+            <XStack className='self-center'>
                 <Input
                     disabled
+                    variant={'lg'}
+                    color={'green'}
+                    className='w-24'
+                    LeadingComponent={<SvgChainNode />}
                     label={<Chip className='bg-decorative-green-200'>Final Output y</Chip>}
                     value={outputValue}></Input>
-            </YStack>
+            </XStack>
         </XStack>
     );
 }
--- src/design-system/input/Input.tsx
+++ src/design-system/input/Input.tsx
@@ -1,22 +1,87 @@
 import { JSX } from 'react';
-import { YStack } from '../layout';
+import { XStack, YStack } from '../layout';
 import { SH3 } from '../typography';
 
+type Variant = 'base' | 'lg';
+type Color = 'neutral' | 'green' | 'yellow';
+
 type Input = {
+    color?: Color;
+    variant?: Variant;
     /**
      * Label for the input component, can be a string or a JSX Component
      */
     label?: string | React.ReactNode;
+    /**
+     * Optional Leading Component on the input field
+     */
+    LeadingComponent?: React.ReactNode;
+    /**
+     * Optional Trailing Component on the input field
+     */
+    TrailingComponent?: React.ReactNode;
 };
 
-export const Input = ({ label, children, ...props }: Input & JSX.IntrinsicElements['input']) => {
+export const Input = ({
+    label,
+    variant = 'base',
+    color = 'neutral',
+    className = '',
+    LeadingComponent = null,
+    TrailingComponent = null,
+    children,
+    ...props
+}: Input & JSX.IntrinsicElements['input']) => {
     return (
-        <YStack className='gap-y-1'>
+        <YStack className='gap-y-1 self-stretch'>
             {Boolean(label) && (typeof label === 'string' ? <SH3>{label}</SH3> : label)}
-            <input
-                className='border border-neutral-400 px-3 py-2 rounded-lg focus:outline-none focus:border focus:border-neutral-500 text-xs leading-3 font-medium text-neutral-700'
-                {...props}
-            />
+            <XStack className='items-center'>
+                {Boolean(LeadingComponent) && (
+                    <XStack
+                        className={`rounded-r-none rounded-lg  h-full flex items-center ${
+                            variant === 'lg' ? 'rounded-xl px-4 border-2' : 'px-3 rounded-lg border'
+                        } ${
+                            color === 'green'
+                                ? 'border-green-400'
+                                : color === 'yellow'
+                                ? 'border-yellow-400'
+                                : 'border-neutral-400'
+                        }`}>
+                        {LeadingComponent}
+                    </XStack>
+                )}
+
+                <input
+                    className={`grow focus:outline-none text-neutral-700 h-full ${
+                        variant === 'lg'
+                            ? 'text-lg leading-5 font-bold py-3 px-4 rounded-xl border-2'
+                            : 'text-xs leading-3 font-medium px-3 py-2  rounded-lg border'
+                    } ${Boolean(TrailingComponent) ? 'border-r-0 rounded-r-none' : ''} ${
+                        Boolean(LeadingComponent) ? 'border-l-0 rounded-l-none' : ''
+                    } ${
+                        color === 'green'
+                            ? 'border-green-400'
+                            : color === 'yellow'
+                            ? 'border-yellow-400'
+                            : 'border-neutral-400'
+                    } ${className}`}
+                    {...props}
+                />
+                {Boolean(TrailingComponent) && (
+                    <XStack
+                        className={`rounded-l-none border-neutral-400 h-full flex items-center ${
+                            variant === 'lg' ? 'rounded-xl px-4 border-2' : 'px-3 rounded-lg border'
+                        }  ${
+                            color === 'green'
+                                ? 'border-green-400'
+                                : color === 'yellow'
+                                ? 'border-yellow-400'
+                                : 'border-neutral-400'
+                        }`}>
+                        {TrailingComponent}
+                    </XStack>
+                )}
+            </XStack>
         </YStack>
     );
 };
